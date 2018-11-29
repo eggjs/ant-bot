8fc55435946cbc4694f72b77b3c21a461d5544db
Haoliang Gao <sakura9515@gmail.com>
2018-11-30 00:43:33 +0800

    feat: add task about event-stream (#5)

---
+++ app/actions/replyEventStream.js
@@ -0,0 +1,31 @@
+const { commentIssue, closeIssue } = require('../../lib/github');
+
+// https://github.com/eggjs/egg/issues/3238
+function matchKeyword(title, body) {
+  if (title.includes('event-stream')) return true;
+  if (body.includes('event-stream')) return true;
+  return false;
+}
+
+function replyEventStream(on) {
+  on('issues_opened', async ({ payload }) => {
+    if (matchKeyword(payload.issue.title, payload.issue.body)) {
+      const content = 'Duplicated #3238';
+
+      commentIssue({
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        number: payload.issue.number,
+        body: content,
+      });
+
+      closeIssue({
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        number: payload.issue.number,
+      });
+    }
+  });
+}
+
+module.exports = replyEventStream;
