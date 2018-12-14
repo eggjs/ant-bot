654f5c4f633071b0c34ade14d090dfdb2820b59e
Haoliang Gao <sakura9515@gmail.com>
2018-12-14 14:47:17 +0800

    feat: improve the instruction for reproduce (#9)

--- app/actions/replyNeedReproduce.js
+++ app/actions/replyNeedReproduce.js
@@ -3,9 +3,15 @@ const { commentIssue } = require('../../lib/github');
 
 const matchedLabel = ['Need Reproduce', 'need-more-information', 'not follow template'];
 const comment = `
-Hello @{user}. Please provide a reproducible example by creating a github repo.
+Hello @{user}. Please provide a reproducible example following the [instruction](https://github.com/eggjs/egg/issues/3310).
 
 Issues labeled by \`Need Reproduce\` will be closed if no activities in 7 days.
+
+---
+
+@{user}，请根据[这个说明](https://github.com/eggjs/egg/issues/3310)提供最小可复现代码。
+
+如果在 7 天内没有进展会被自动关闭。
 `;
 
 function replyNeedReproduce(on) {
